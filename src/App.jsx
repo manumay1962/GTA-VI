7ddc84bf55afbdfed8f3503428a61be981833c1d
manumay@1962 <manumay8@gmail.com>
2025-05-11 18:09:59 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useGSAP } from '@gsap/react'
+import React from 'react'
+import { gsap } from 'gsap'
+import { Timeline } from 'gsap/gsap-core'
+
+
+function App() {
+  useGSAP(() => {
+    gsap.to('.vi-mask-group', {
+      
+      
+      duration: 2,
+      rotate: 10,
+      ease: 'power2.inOut'
+    })
+  }, [])
+  return (
+   <>
+         <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
+        <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
+          <defs>
+            <mask id="viMask">
+              <rect width="100%" height="100%" fill="black" />
+              <g className="vi-mask-group">
+                <text
+                  x="50%"
+                  y="50%"
+                  fontSize="250"
+                  textAnchor="middle"
+                  fill="white"
+                  dominantBaseline="middle"
+                  fontFamily="Arial Black"
+                >
+                  VI
+                </text>
+              </g>
+            </mask>
+          </defs>
+          <image
+            href="./bg.png"
+            width="100%"
+            height="100%"
+            preserveAspectRatio="xMidYMid slice"
+            mask="url(#viMask)"
+          />
+        </svg>
+      </div>
+   </>
+  )
+}
+
+export default App
